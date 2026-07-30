a550a1b86f230c1b69e1e8c8b34cd83ba33f2bfb
Peter Galantha <19354642+peterg79@users.noreply.github.com>
2026-07-29 19:52:38 -0700

    more ios

--- script.js
+++ script.js
@@ -13,33 +13,33 @@ if (retries > 100) {
 
 
 function showTopMessage(text, bgcolor, textcolor) {
-  // 1. Create the message container
-  //const msgBox = document.createElement('div');
-  msgBox.id = 'dynamic-message-top';
-  msgBox.classList.add('msgbox');
- 
-  if (bgcolor) {
-    msgBox.style.backgroundColor = bgcolor;
-  } else {
-    msgBox.style.backgroundColor = '#ffcc00'; // Eye-catching color
-  }
-  if (textcolor) {
-    msgBox.style.color = textcolor;
-  } else {
-    msgBox.style.color = '#333333';
-  }
- 
-  // 2. Add text and append to body
-  //msgBox.innerText = text;
-  msgBox.innerHTML = text;
-  document.body.prepend(msgBox);
-  
-  // 3. Remove it after 30 seconds
-  /*
-  setTimeout(() => {
-    msgBox.remove();
-  }, 30000);
-  */
+    // 1. Create the message container
+    //const msgBox = document.createElement('div');
+    msgBox.id = 'dynamic-message-top';
+    msgBox.classList.add('msgbox');
+
+    if (bgcolor) {
+        msgBox.style.backgroundColor = bgcolor;
+    } else {
+        msgBox.style.backgroundColor = '#ffcc00'; // Eye-catching color
+    }
+    if (textcolor) {
+        msgBox.style.color = textcolor;
+    } else {
+        msgBox.style.color = '#333333';
+    }
+
+    // 2. Add text and append to body
+    //msgBox.innerText = text;
+    msgBox.innerHTML = text;
+    document.body.prepend(msgBox);
+
+    // 3. Remove it after 30 seconds
+    /*
+    setTimeout(() => {
+      msgBox.remove();
+    }, 30000);
+    */
 }
 
 const form = document.getElementById('form');
@@ -53,90 +53,90 @@ var score;
 
 function fillContent() {
 
-input.value = '';
+    input.value = '';
 
-let pnum1 = localStorage.getItem('num1');
-let pnum2 = localStorage.getItem('num2');
-let puserAnswer = localStorage.getItem('userAnswer');
+    let pnum1 = localStorage.getItem('num1');
+    let pnum2 = localStorage.getItem('num2');
+    let puserAnswer = localStorage.getItem('userAnswer');
 
-//console.log(`${pnum1} ${pnum2} ${puserAnswer}`);
+    //console.log(`${pnum1} ${pnum2} ${puserAnswer}`);
 
-if (pnum1 && pnum2 && puserAnswer) {
-    let pcorrectAnswer = parseInt(pnum1) * parseInt(pnum2);
-    if (pcorrectAnswer == parseInt(puserAnswer)) {
-        //showTopMessage('<p style="font-size: 28px;">CORRECT!  🙂</p>', 'green', 'white')
-        //showTopMessage('<p style="font-size: 28px;">CORRECT!</p>', 'green', 'white')
-        showTopMessage(`<p style="font-size: 28px;">CORRECT!</p>${pnum1} x ${pnum2} = ${pcorrectAnswer}`, 'green', 'white')
-    } else {
-        showTopMessage(`${pnum1} x ${pnum2} =<p style="font-size: 28px;">${pcorrectAnswer}</p>`, 'red', 'white')
-    }
-}
-
-localStorage.removeItem("num1");
-localStorage.removeItem("num2");
-localStorage.removeItem("userAnswer");
-
-
-
-
-
-
-
-
-let maxPlus = max + 1;
-goodanswers = [];
-for (let i = 0; i < maxPlus; i++) {
-  goodanswers.push(new Array(maxPlus).fill(0));
-}
-
-goodanswersJson = localStorage.getItem('goodanswers');
-if(goodanswersJson) {
-
-    goodanswers = JSON.parse(goodanswersJson);
-    for (i=0; i<goodanswers.length; i++) {
-        console.log(goodanswers[i]);
-    }
-    if (goodanswers.length < maxPlus) {
-        for (i=0; i<goodanswers.length; i++) {
-            goodanswers[i].push(...new Array(maxPlus - goodanswers.length).fill(0));
-        }
-        for (i=goodanswers.length; i < maxPlus; i++) {
-            goodanswers.push(new Array(maxPlus).fill(0));
+    if (pnum1 && pnum2 && puserAnswer) {
+        let pcorrectAnswer = parseInt(pnum1) * parseInt(pnum2);
+        if (pcorrectAnswer == parseInt(puserAnswer)) {
+            //showTopMessage('<p style="font-size: 28px;">CORRECT!  🙂</p>', 'green', 'white')
+            //showTopMessage('<p style="font-size: 28px;">CORRECT!</p>', 'green', 'white')
+            showTopMessage(`<p style="font-size: 20px;">CORRECT!</p>${pnum1} x ${pnum2} = ${pcorrectAnswer}`, 'green', 'white')
+        } else {
+            showTopMessage(`<p style="font-size: 20px;">${pnum1} x ${pnum2} = ${pcorrectAnswer}</p>`, 'red', 'white')
         }
     }
-}
 
-// find min
-let minGoodAnswers = 9999999999;
-for (i=min; i<maxPlus; i++) {
-    for (j=min; j<maxPlus; j++) {
-        if (goodanswers[i][j] < minGoodAnswers) {
-            minGoodAnswers = goodanswers[i][j];
+    localStorage.removeItem("num1");
+    localStorage.removeItem("num2");
+    localStorage.removeItem("userAnswer");
+
+
+
+
+
+
+
+
+    let maxPlus = max + 1;
+    goodanswers = [];
+    for (let i = 0; i < maxPlus; i++) {
+        goodanswers.push(new Array(maxPlus).fill(0));
+    }
+
+    goodanswersJson = localStorage.getItem('goodanswers');
+    if (goodanswersJson) {
+
+        goodanswers = JSON.parse(goodanswersJson);
+        for (i = 0; i < goodanswers.length; i++) {
+            console.log(goodanswers[i]);
+        }
+        if (goodanswers.length < maxPlus) {
+            for (i = 0; i < goodanswers.length; i++) {
+                goodanswers[i].push(...new Array(maxPlus - goodanswers.length).fill(0));
+            }
+            for (i = goodanswers.length; i < maxPlus; i++) {
+                goodanswers.push(new Array(maxPlus).fill(0));
+            }
         }
     }
+
+    // find min
+    let minGoodAnswers = 9999999999;
+    for (i = min; i < maxPlus; i++) {
+        for (j = min; j < maxPlus; j++) {
+            if (goodanswers[i][j] < minGoodAnswers) {
+                minGoodAnswers = goodanswers[i][j];
+            }
+        }
+    }
+
+    num1 = num2 = correctAnswer = 0;
+    for (i = 0; i < retries; i++) {
+        num1 = Math.floor(Math.random() * (maxPlus - min)) + min;
+        num2 = Math.floor(Math.random() * (maxPlus - min)) + min;
+        correctAnswer = num1 * num2;
+        if (goodanswers[num1][num2] <= minGoodAnswers) break;
+    }
+
+
+    questionElement.innerText = `What is ${num1} Multiply by ${num2}?`;
+    questionElement.innerText = `${num1} x ${num2} = ?`;
+
+    score = Number(localStorage.getItem("score"));
+    if (!score) {
+        score = 0;
+    }
+
+    scoreElement.textContent = `score : ${score}`;
 }
 
-num1 = num2 = correctAnswer = 0;
-for (i=0; i < retries; i++) {
-    num1 = Math.floor(Math.random() * (maxPlus-min)) + min;
-    num2 = Math.floor(Math.random() * (maxPlus-min)) + min;
-    correctAnswer = num1 * num2;
-    if (goodanswers[num1][num2] <= minGoodAnswers) break;
-}
-
-
-questionElement.innerText = `What is ${num1} Multiply by ${num2}?`;
-questionElement.innerText = `${num1} x ${num2} = ?`;
-
-score = Number(localStorage.getItem("score"));
-if(!score) {
-    score = 0;
-}
-
-scoreElement.textContent = `score : ${score}`;
-}
-
-form.addEventListener('submit',function() {
+form.addEventListener('submit', function () {
 
     event.preventDefault();
     if (input.value.trim() === '') {
@@ -149,7 +149,7 @@ form.addEventListener('submit',function() {
     localStorage.setItem('num2', String(num2));
     localStorage.setItem('userAnswer', String(userAnswer));
 
-    if(correctAnswer === userAnswer) {
+    if (correctAnswer === userAnswer) {
         score++;
         updateScore(1);
     }
@@ -157,11 +157,12 @@ form.addEventListener('submit',function() {
         //score--;
         updateScore(-1);
     }
+    input.focus();
     fillContent();
 });
 
 function updateScore(arg) {
-    localStorage.setItem("score",String(score));
+    localStorage.setItem("score", String(score));
     goodanswers[num1][num2] += arg;
     localStorage.setItem('goodanswers', JSON.stringify(goodanswers));
 }
@@ -171,6 +172,7 @@ reset.addEventListener('click', () => {
     event.preventDefault();
     localStorage.removeItem("score");
     localStorage.removeItem("goodanswers");
+    input.focus();
     //window.location.reload();
     fillContent();
 });
@@ -178,6 +180,7 @@ reset.addEventListener('click', () => {
 const reload = document.getElementById('reload');
 reload.addEventListener('click', () => {
     event.preventDefault();
+    input.focus();
     //window.location.reload();
     fillContent();
 });
@@ -188,20 +191,20 @@ fillContent();
 //localStorage.removeItem("score");
 
 
-keypad.addEventListener('click', function(event) {
-  // Ignore clicks that aren't on dialpad buttons
-  const btn = event.target.closest('button');
-  if (!btn) return;
+keypad.addEventListener('click', function (event) {
+    // Ignore clicks that aren't on dialpad buttons
+    const btn = event.target.closest('button');
+    if (!btn) return;
 
-  const val = btn.dataset.val;
+    const val = btn.dataset.val;
 
-  if (val === 'clear') {
-    input.value = '';
-  } else if (val === 'backspace') {
-    input.value = input.value.slice(0, -1);
-  } else {
-    input.value += val;
-  }
+    if (val === 'clear') {
+        input.value = '';
+    } else if (val === 'backspace') {
+        input.value = input.value.slice(0, -1);
+    } else {
+        input.value += val;
+    }
 
-  input.focus();
+    input.focus();
 });
